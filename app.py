a29d9fef43bdf17495916026166d68f50ed6e44a
CameronChilversWMA <Cameron.s.chilvers@student.uts.edu.au>
2023-05-03 13:08:55 +1000

    Take a picture

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, send_from_directory, url_for, flash, redirect
 import json
-from flask_uploads import UploadSet, IMAGES, configure_uploads
 from werkzeug.utils import secure_filename
 import os
 
