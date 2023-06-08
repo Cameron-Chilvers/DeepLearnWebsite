ccec4293e179257004ee747e429d7cd19bb0b38a
CameronChilversWMA <Cameron.s.chilvers@student.uts.edu.au>
2023-06-08 13:20:46 +1000

    final updates

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def save_uploaded_photo():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             filename = os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER'], filename)
             
-            saveDir = run(PTPATH, source=filename, data=YAMLPATH, line_thickness=1)
+            saveDir = run(PTPATH, source=filename, data=YAMLPATH, line_thickness=2)
             shutil.move(os.path.join(saveDir, secure_filename(file.filename)), filename)
             filename = '\\uploads\\'+ secure_filename(file.filename)
 
@@ -72,7 +72,7 @@ def save_taken_photo():
 
         photo_file.save(file_path)
 
-        saveDir = run(PTPATH, source=file_path, data=YAMLPATH, line_thickness=2)
+        saveDir = run(PTPATH, source=file_path, data=YAMLPATH, line_thickness=1, conf_thres=0.35)
         shutil.move(os.path.join(saveDir, filename), file_path)
         filename = os.path.join('uploads', filename)
 
--- camera.py
+++ camera.py
@@ -4,11 +4,12 @@ import torch
 from yolov5.detect import run
                 
 model = torch.hub.load('yolov5','custom', path='model.pt', force_reload=True, source='local')
+model.conf = 0.35  # confidence threshold (0-1)
 
 class VideoCamera(object):
     def __init__(self):
        # Setting Video captyure to be webcam
-        self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture(2)
 
     
     def __del__(self):
